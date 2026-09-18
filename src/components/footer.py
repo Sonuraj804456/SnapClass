d21328dde63de3160adb4b74a6e3533cc4e2a9f4
SnapClass Local <snapclass@example.com>
2026-09-18 16:04:25 +0530

    UI: use provided image URL in footer

--- src/components/footer.py
+++ src/components/footer.py
@@ -2,24 +2,24 @@ import streamlit as st
 
 
 def footer_home():
-    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
+    logo_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDKfY1LzMYmj7yZuWy-7LC1i_PWXPulOCoJtIZRaicMg&s=10"
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
         <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>  
-        <span style="display:inline-block; background:#ffffff22; color:#fff; padding:4px 10px; border-radius:999px; font-weight:700; margin-left:8px;">SONU</span>
+        <img src='{logo_url}' style='max-height:25px; margin-left:8px;' />
         </div>
                 
                 """, unsafe_allow_html=True)
 
 
 def footer_dashboard():
-    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
+    logo_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDKfY1LzMYmj7yZuWy-7LC1i_PWXPulOCoJtIZRaicMg&s=10"
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
         <p style="font-weight:bold; color:black;"> Created with ❤️ by </p>  
-        <span style="display:inline-block; background:#0000000f; color:#000; padding:4px 10px; border-radius:999px; font-weight:700; margin-left:8px;">SONU</span>
+        <img src='{logo_url}' style='max-height:25px; margin-left:8px;' />
         </div>
                 
                 """, unsafe_allow_html=True)
